bfbeee9bb1039107495b38453647ac422a68df0f
hexoN <aliakbaripour00@gmail.com>
2024-03-02 15:14:33 +0330

    Clearing the code Add functions and calling them onclick

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let cartQuantity = 0;
+
+function updateCartQuantityHigher(value) {
+  if (cartQuantity + value > 10) {
+    alert('You can not store that many items in your cart.');
+    return;
+  }
+  cartQuantity += value;
+  console.log(`Cart Quantity: ${cartQuantity}`);
+}
+
+function updateCartQuantityLower(value) {
+  if (cartQuantity - value < 0) {
+    alert('You do not have any items in your cart to remove.');
+    return;
+  }
+  cartQuantity -= value;
+  console.log(`Cart Quantity: ${cartQuantity}`);
+}

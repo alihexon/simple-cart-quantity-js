f92f08c33fe3e01b851d9e32003e056335c3674e
hexoN <aliakbaripour00@gmail.com>
2024-03-04 14:53:49 +0330

    DOM Update The cart quantity now displays on the webpage

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let cartQuantity = 0;
+const cartQuantityCounter = document.querySelector('.cart-quantity-counter');
 
 function updateCartQuantityHigher(value) {
+  const cartQuantityCounter = document.querySelector('.cart-quantity-counter');
+
   if (cartQuantity + value > 10) {
     alert('You can not store that many items in your cart.');
     return;
   }
   cartQuantity += value;
-  console.log(`Cart Quantity: ${cartQuantity}`);
+  cartQuantityCounter.innerHTML = `Cart Quantity: ${cartQuantity}`;
 }
 
 function updateCartQuantityLower(value) {
+
   if (cartQuantity - value < 0) {
     alert('You do not have any items in your cart to remove.');
     return;
   }
   cartQuantity -= value;
-  console.log(`Cart Quantity: ${cartQuantity}`);
+  cartQuantityCounter.innerHTML = `Cart Quantity: ${cartQuantity}`;
+}
+
+function updateCounterOnReset() {
+  cartQuantity = 0;
+  cartQuantityCounter.innerHTML = `The cart was reset. Cart Quantity: ${cartQuantity}`;
 }
